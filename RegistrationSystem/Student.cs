ecc74309675ba2b5e24f2f7123c620c18aad2053
Robert Mcguire <Dev_Robert@protonmail.com>
2018-09-12 15:32:07 -0400

    FDFD

--- RegistrationSystem/Student.cs
+++ RegistrationSystem/Student.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RegistrationSystem
+namespace RegistrationSystem 
 {
     class Student : Person{
         //properties that student has
